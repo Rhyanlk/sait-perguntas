b8ccf760843bb8eaed574ae62fef954e0c5e4742
Rhyanlk <167093663+Rhyanlk@users.noreply.github.com>
2024-11-04 12:28:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+  'Free Fire ',
+  'Qual personagem restaura vida?',
+  'Alok'
+)
+
+criaCartao(
+    'Free Fire ',
+    'Qual a maior patente?',
+    'Desafiante'
+  )
+
+  criaCartao(
+    'Free Fire',
+    'Qual personagem que silencia os rifles?',
+    'Rafael'
+  )
+
+  criaCartao(
+    'Free Fire',
+    'Qual foi o primeiro campeao do mundial de ff',
+    'Corinthians'
+  )
